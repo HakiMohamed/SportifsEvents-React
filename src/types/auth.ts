bf496915a8fe330fc22fa0d4ffbbe1cc14683cbd
Mohamed Haki <Mohamedhaki70@gmail.com>
2024-11-19 15:41:10 +0100

    Ajout des types pour l'authentification (auth.ts)

---
+++ src/types/auth.ts
@@ -0,0 +1,23 @@
+// src/types/auth.ts
+
+
+export interface SignupData {
+    username: string;
+    email: string;
+    password: string;
+  }
+  
+  export interface SigninData {
+    email: string;
+    password: string; 
+  }
+  
+  export interface UserResponse {
+    user: {
+      id: string;
+      username: string;
+      email: string;
+      roles?: string[];
+    };
+    access_token: string;
+  }
